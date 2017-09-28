d39951ee213849d026af9b9ccef6758300bdad55
tjk9b <31296885+tjk9b@users.noreply.github.com>
2017-09-28 13:39:37 -0500

    Awesome commit message here

--- GitWorlflow/ViewController.swift
+++ GitWorlflow/ViewController.swift
@@ -9,11 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+//awesom preogramming
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
